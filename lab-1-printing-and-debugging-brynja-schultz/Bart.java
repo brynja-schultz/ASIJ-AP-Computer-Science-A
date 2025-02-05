ac0e013875bd140ba6a9d847ffde84e27b9ce206
Brynja Schultz <54891716+brynja-schultz@users.noreply.github.com>
2025-02-04 19:36:18 -0700

    Add files via upload

---
+++ lab-1-printing-and-debugging-brynja-schultz/Bart.java
@@ -0,0 +1,20 @@
+public class Bart
+{
+   public static void main(String args[])
+   {
+      System.out.println(" /\\/\\/\\/\\/\\/\\/\\");
+		System.out.println(":              :");
+		System.out.println("|              |   Bart");
+		System.out.println("|              |");
+		System.out.println("|              |");
+		System.out.println("|            __`,");
+		System.out.println("|           /   \\");
+		System.out.println("|           \\__*/__");
+		System.out.println(":               ___)");
+		 System.out.println("\\              \\");
+		  System.out.println(":      _______/");
+		  System.out.println("|         /");
+		  System.out.println(";        :");
+		 System.out.println(";         ; SSt");
+   }
+}
---
+++ lab-1-printing-and-debugging-brynja-schultz/PrintMonth.java
@@ -0,0 +1,8 @@
+public class PrintMonth
+{
+   public static void main(String args[])
+   {
+   System.out.print("August");
+   }
+   
+}
