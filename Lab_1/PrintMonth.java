122cd4ee9bd7cd8d040d91a30321fceafb70542a
Brynja Schultz <54891716+brynja-schultz@users.noreply.github.com>
2025-02-04 19:36:02 -0700

    Delete Lab_1 directory

--- Lab_1/PrintMonth.java
+++
@@ -1,8 +0,0 @@
-public class PrintMonth
-{
-   public static void main(String args[])
-   {
-   System.out.print("August");
-   }
-   
-}
--- Lab_1/PrintZipCode.java
+++
@@ -1,8 +0,0 @@
-public class PrintZipCode 
-{
-   // method main(): program starting point
-   public static void main( String[] args ) 
-   {
-      System.out.print(1060046);   
-   }
-}
